96bc12f24a10d67ef2fd2c19e42b2e8e3de4d6a2
pji0219 <pji219@naver.com>
2021-12-18 22:40:30 +0900

    요청

---
+++ 6-express/index.js
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/sky/:id', (req, res, next) => {
+  console.log(req.path);
+  console.log(req.headers);
+  console.log(req.params); // /id
+  console.log(req.params.id); // /id, 접근 req.params.id`
+  console.log(req.query); // /?keyword=kimsohyun
+  console.log(req.query.keyword); // 접근 req.query.keyword
+  res.send('hi');
+});
+
+app.listen(8080);
