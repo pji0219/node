7942f5c573e164ac3ac4dc9db0190c16daabd9ee
pji0219 <pji219@naver.com>
2021-12-27 23:49:33 +0900

    응답

--- 6-express/index.js
+++ 6-express/index.js
@@ -12,7 +12,9 @@ app.get('/sky/:id', (req, res, next) => {
   console.log(req.query); // /?keyword=kimsohyun
   console.log(req.query.keyword); // 접근 req.query.keyword
 
-  res.send({ name: 'pji' });
+  // res.send({ name: 'pji' }); 데이터를 응답
+  // res.sendStatus(200); status를 응답
+  // res.status(201).send('created'); status 코드와 메세지를 응답
 });
 
 app.listen(8080);
