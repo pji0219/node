7aefcdf081aa5f32a05cd77242e74490972586de
pji0219 <pji219@naver.com>
2022-01-06 22:22:07 +0900

    미들웨어

--- 6-express/study/1.req.js
+++ 6-express/study/1.req.js
@@ -1,10 +1,11 @@
-// 요청
+// 요청과 응답
 
 import express from 'express';
 
 const app = express();
 
 app.get('/sky/:id', (req, res, next) => {
+  // req
   console.log(req.path);
   console.log(req.headers);
   console.log(req.params); // /id
@@ -12,7 +13,11 @@ app.get('/sky/:id', (req, res, next) => {
   console.log(req.query); // /?keyword=kimsohyun!
   console.log(req.query.keyword); // 접근 req.query.keyword
 
-  res.send({ name: 'pji' });
+  // res
+  // res.send({ name: 'pji' });
+  // res.sendStatus(200);
+  res.setHeader('key', 'value');
+  res.status(201).send('created');
 });
 
 app.listen(8080);
