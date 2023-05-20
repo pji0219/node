010a191112c5897fc5b6882494b232ca46d60f80
pji0219 <pji219@naver.com>
2023-05-20 23:24:43 +0900

    미들 웨어 특징

---
+++ remind/6-express/app.js
@@ -0,0 +1,64 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/node/:id', (req, res, next) => {
+  // console.log(req.path);
+  // console.log(req.headers);
+  console.log(req.params);
+  console.log(req.params.id);
+  console.log(req.query);
+  console.log(req.query.job);
+
+  // res.send('Hello, World!');
+  // res.json({ name: 'pji' });
+  // res.sendStatus(200);
+  // res.status(201).send('created!');
+  // res.setHeader('key', 'value');
+  res.status(201).json({ name: 'pji' });
+});
+
+// all 과 use 모두 모든 http메서드를 들을 수 있지만
+// all은 /api 뒤에 없는 경로 입력 시 처리가 안됨
+app.all('/api', (req, res, next) => {
+  console.log('all');
+  res.send('all');
+});
+
+// use /sky 뒤에 없는 경로 입력 시에도 처리 됨
+app.use('/sky', (req, res, next) => {
+  console.log('use');
+  res.send('use');
+});
+
+app.get(
+  '/',
+  (req, res, next) => {
+    console.log('first!');
+    next('route'); // 다음 미들웨어 무시하고 그 다음 미들웨어로
+    // next(new Error());
+  },
+  (req, res, next) => {
+    console.log('first2!');
+    next();
+  }
+);
+
+app.get('/', (req, res, next) => {
+  console.log('second!');
+  if (true) {
+    return res.send('second!1');
+  }
+  res.send('second!2');
+});
+
+app.use((req, res, next) => {
+  res.status(404).send('Not Found!');
+});
+
+app.use((error, req, res, next) => {
+  console.error(error);
+  res.status(500).send('try later!');
+});
+
+app.listen(8080, () => {});
