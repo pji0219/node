8acf719de6d201de7dacf51ba9aee13d2305b450
pji0219 <pji219@naver.com>
2023-05-21 02:13:20 +0900

    에러 처리

--- remind/6-express/app.js
+++ remind/6-express/app.js
@@ -1,7 +1,9 @@
 import express from 'express';
-
+import fs from 'fs';
+import fsAsync from 'fs/promises';
 const app = express();
 
+// 요청 및 응답
 app.get('/node/:id', (req, res, next) => {
   // console.log(req.path);
   // console.log(req.headers);
@@ -17,6 +19,8 @@ app.get('/node/:id', (req, res, next) => {
   // res.setHeader('key', 'value');
   res.status(201).json({ name: 'pji' });
 });
+
+// 미들웨어 동작
 
 // all 과 use 모두 모든 http메서드를 들을 수 있지만
 // all은 /api 뒤에 없는 경로 입력 시 처리가 안됨
@@ -52,10 +56,45 @@ app.get('/', (req, res, next) => {
   res.send('second!2');
 });
 
+// post 처리
 app.use(express.json()); // body에 들어오는 것을 파싱해서 읽도록 함
 
 app.post('/', (req, res, next) => {
   console.log(req.body);
+});
+
+// 에러 처리의 모든 것(동기, 비동기) 4.x 버전
+app.get('/file', (req, res, next) => {
+  fs.readFile('/file1.txt', (err, data) => {
+    if (err) {
+      res.sendStatus(404);
+    }
+  });
+});
+
+app.get('/file1', (req, res, next) => {
+  try {
+    const data = fs.readFileSync('/file1.txt');
+    console.log(data);
+  } catch (error) {
+    res.sendStatus(404);
+  }
+});
+
+app.get('/file2', (req, res, next) => {
+  fsAsync
+    .readFile('file2.txt') //
+    .then((data) => res.send(data))
+    .catch((error) => res.sendStatus(404));
+});
+
+app.get('/file3', async (req, res, next) => {
+  try {
+    const data = await fsAsync.readFile('file2.txt');
+    res.send(data);
+  } catch (error) {
+    res.sendStatus(404);
+  }
 });
 
 app.use((req, res, next) => {
