5c9834627879f329be589301d6681ae0868d110e
pji0219 <pji219@naver.com>
2023-05-21 18:44:37 +0900

    내부 미들웨어

--- remind/6-express/app.js
+++ remind/6-express/app.js
@@ -1,10 +1,24 @@
 import express from 'express';
+
 import postRouter from './router/post.js';
 import userRouter from './router/user.js';
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json()); // REST API -> Body parsing
+app.use(express.urlencoded({ extended: false })); // HTML Form -> Body parsing
+
+const options = {
+  dotfiles: 'ignore', // 숨김 파일 안보이게
+  etag: false,
+  index: false,
+  maxAge: '1d', // 캐시 얼마 기간 동안 유지
+  redirect: false,
+  setHeaders: function (res, path, stat) {
+    res.set('x-timestamp', Date.now()); // 헤더에 추가
+  },
+};
+app.use(express.static('public', options)); // public폴더에 있는 것을 접근하기 위한 것
 
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
