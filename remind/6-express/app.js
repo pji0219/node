31b812e989cb9aa0cb67cd0ab98f18269634b04f
pji0219 <pji219@naver.com>
2023-05-24 22:07:48 +0900

    외부 미들웨어들

--- remind/6-express/app.js
+++ remind/6-express/app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 import postRouter from './router/post.js';
 import userRouter from './router/user.js';
@@ -7,6 +10,9 @@ const app = express();
 
 app.use(express.json()); // REST API -> Body parsing
 app.use(express.urlencoded({ extended: false })); // HTML Form -> Body parsing
+app.use(cookieParser());
+app.use(morgan('tiny')); // comnined이 기본 포맷, 요청 등등 각종 정보 표시하는 미들웨어
+app.use(helmet()); // 공통적으로 보안에 필요한 헤더를 추가해줌
 
 const options = {
   dotfiles: 'ignore', // 숨김 파일 안보이게
@@ -22,6 +28,13 @@ app.use(express.static('public', options)); // public폴더에 있는 것을 접
 
 app.use('/posts', postRouter);
 app.use('/users', userRouter);
+
+app.get('/', (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
+  console.log(req.cookies.yummy_cookie);
+  res.send('Hi');
+});
 
 app.use((req, res) => {
   res.status(404).send('Not Found!');
